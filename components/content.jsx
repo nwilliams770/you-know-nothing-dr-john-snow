ac84c08b6a0e2bd62ddcaee27b03fad08b6de0a6
Nicholas Williams <nwilliams770@gmail.com>
2019-01-22 11:04:37 -0800

    add contour and voronoi modules

--- components/content.jsx
+++ components/content.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { feature } from "topojson-client";
 import MapModule from './map_module';
+import VoronoiModule from './voronoi_module';
+import ContourModule from './contour_module';
 
 class Content extends React.Component {
     constructor() {
@@ -34,7 +36,9 @@ class Content extends React.Component {
             let data = await Promise.all([
                 fetch('/soho_1854.json').then(response => response.json()),
                 fetch('/pumps-topo.json').then(response => response.json()),
-                fetch('/deaths-topo.json').then(response => response.json())
+                fetch('/deaths-topo.json').then(response => response.json()),
+                fetch('/test.json').then(response => response.json())
+
             ]);
             return data;
         } catch(err) {
@@ -44,18 +48,41 @@ class Content extends React.Component {
 
     render() {
         // This is where we can add a spinner
+        // TO DO: Perhaps this is where we should add the projection, and then just pass it down
         if (!this.state.isDataFetched) return null;
-        const { houses, borders, roadLabels, placeLabels, pumps, deaths } = this.state;
+        const width = 960
+            , height = 800;
+        const { houses, borders, roadLabels, placeLabels, pumps, deaths, testPumps } = this.state;
         return (
+            <svg width={`${width}`} height={`${height}`} viewBox={`0 0 ${width} ${height}`}>
+                <MapModule
+                    houses={houses}
+                    borders={borders}
+                    roadLabels={roadLabels}
+                    placeLabels={placeLabels}
+                    pumps={pumps}
+                    deaths={deaths}
+                    width={width}
+                    height={height}
+                />
+                <VoronoiModule
+                    pumps={pumps}
+                    width={width}
+                    height={height}
+                    testPumps={testPumps}
+                    deaths={deaths}
+                />
+                <ContourModule
+                    deaths={deaths}
+                    width={width}
+                    height={height}
+                />
+            </svg>
+            // To-do:
+            // We want to have all the elements in the same bounding box
+            // So in content, we can have an svg wrapper, and moodules will just return wrapped
+            // in a <g> tag, which is just the div of svgs
             // Maybe just pass {...this.state}? 
-            <MapModule
-                houses={houses}
-                borders={borders}
-                roadLabels={roadLabels}
-                placeLabels={placeLabels}
-                pumps={pumps}
-                deaths={deaths}
-             />
         )
     }
 }
