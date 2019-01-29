5678abb48e149e1b92b5c37ea26dd3e362b2d50e
Nicholas Williams <nwilliams770@gmail.com>
2019-01-28 16:36:53 -0800

    add tooltip

---
+++ components/tooltip.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export function showPumpTooltip(evt) {
+    const tooltip = document.querySelector(".tooltip");
+    tooltip.innerHTML = evt.target.dataset.title.replace("/", "X");
+    tooltip.style.display = "block";
+    tooltip.style.left = evt.pageX + 10 + 'px';
+    tooltip.style.top = evt.pageY + 10 + 'px';
+}
+
+export function showVoronoiTooltip(evt) {
+    // CSS :hover was jerky, trying this for a cleaner UX
+    const cell = evt.target;
+    cell.style.fill = "#888888";
+    cell.style.fillOpacity = "0.3";
+
+    const tooltip = document.querySelector(".tooltip");
+    const deathCount = tooltip.childNodes[0];
+    const copy = tooltip.childNodes[1];
+    deathCount.innerHTML = evt.target.dataset.deaths;
+    copy.innerHTML = "deaths from Cholera";
+
+    tooltip.style.display = "block";
+    tooltip.style.left = evt.pageX + 5 + 'px';
+    tooltip.style.top = evt.pageY + 5 + 'px';
+    return
+}
+  
+export function hideTooltip(evt) {
+    const tooltip = document.querySelector(".tooltip");
+    tooltip.style.display = "none";
+
+    // if a Voronoi tooltip, must revert cell fill back
+    if (evt.target.dataset.hasOwnProperty("deaths")) {
+        evt.target.style.fill = "transparent";
+    }
+    return
+}
+
+const Tooltip = () => (
+    <div className="tooltip"><h2 className="voronoi-count"></h2><p></p></div>
+)
+
+export default Tooltip;
