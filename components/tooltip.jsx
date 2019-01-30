55f4d43e1af481993b4fa7f2e43a385862bf711f
Nicholas Williams <nwilliams770@gmail.com>
2019-01-29 16:38:34 -0800

    update tooltip placement

--- components/tooltip.jsx
+++ components/tooltip.jsx
@@ -5,7 +5,7 @@ export function showPumpTooltip(evt) {
     tooltip.innerHTML = evt.target.dataset.title.replace("/", "X");
     tooltip.style.display = "block";
     tooltip.style.left = evt.pageX + 10 + 'px';
-    tooltip.style.top = evt.pageY + 10 + 'px';
+    tooltip.style.top = evt.pageY - 25 + 'px';
 }
 
 export function showVoronoiTooltip(evt) {
@@ -17,12 +17,13 @@ export function showVoronoiTooltip(evt) {
     const tooltip = document.querySelector(".tooltip");
     const deathCount = tooltip.childNodes[0];
     const copy = tooltip.childNodes[1];
+    tooltip.classList.add("voronoi-format");
     deathCount.innerHTML = evt.target.dataset.deaths;
-    copy.innerHTML = "deaths from Cholera";
+    copy.innerHTML = evt.target.dataset.deaths != 1 ? "deaths from Cholera" : "death from Cholera";
 
     tooltip.style.display = "block";
-    tooltip.style.left = evt.pageX + 5 + 'px';
-    tooltip.style.top = evt.pageY + 5 + 'px';
+    tooltip.style.left = evt.pageX + 10 + 'px';
+    tooltip.style.top = evt.pageY - 25 + 'px';
     return
 }
   
@@ -33,6 +34,8 @@ export function hideTooltip(evt) {
     // if a Voronoi tooltip, must revert cell fill back
     if (evt.target.dataset.hasOwnProperty("deaths")) {
         evt.target.style.fill = "transparent";
+        tooltip.classList.remove("voronoi");
+
     }
     return
 }
